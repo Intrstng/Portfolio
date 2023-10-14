f84b941425ca54cf76a305ebf7cd49c93e3ea30e
Intrstng <intrstg@gmail.com>
2023-10-15 00:17:23 +0300

    refactor: change Icon component for common type iconsSprite

--- src/Components/icon/Icon.tsx
+++ src/Components/icon/Icon.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import iconLogo from '../../assets/images/logo.svg';
 import iconsSprite from '../../assets/images/icons-sprite.svg';
 
 type IconPropsType = {
     iconId: 'logo' | 'instagram' | 'instagramActive' | 'telegram' | 'telegramActive' | 'twitter' | 'twitterActive' | 'facebook' | 'facebookActive'
+    width?: string
+    height?: string
+    viewBox?: string
 }
 
 export const Icon = (props: IconPropsType) => {
-
-    if (props.iconId === 'logo') {
-        return (
-            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <use xlinkHref={ `${iconLogo}#${props.iconId}` }/>
-            </svg>
-        );
-    } return (
-        <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <use xlinkHref={ `${iconsSprite}#${props.iconId}` }/>
+    return (
+        <svg width={props.width || '25'} height={props.height || '25'} viewBox={props.viewBox || '0 0 25 25'} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
         </svg>
     );
 };
--- src/Components/logo/Logo.tsx
+++ src/Components/logo/Logo.tsx
@@ -5,7 +5,10 @@ import {Icon} from "../icon/Icon";
 export  const Logo = () => {
     return (
         <a href="#">
-            <Icon iconId={'logo'}/>Creativeart
+            <Icon iconId={'logo'}
+                  width={'28'}
+                  height={'28'}
+                  viewBox={'0 0 28 28'}/>Creativeart
         </a>
     );
 };
